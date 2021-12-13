414b1fda2cda56a655de995312d0fbae1af463e0
nikeshhbaskaran01@gmail.com <nikeshhbaskaran01@gmail.com>
2021-12-13 06:00:34 -0500

    new random concepts

--- random/functions.py
+++ random/functions.py
@@ -4,3 +4,20 @@ def mean(mylist):
     return the_mean
 
 print(mean([1, 4, 5]))
+
+# Conditionals
+def mean_with_condition(value):
+    if type(value) == dict:
+        the_mean = sum(value.values()) / len(value)
+    elif type(value) == list:
+        the_mean = sum(value) / len(value)
+    else:
+        the_mean = None
+    return the_mean
+
+print(mean_with_condition([1, 4, 5]))
+print(mean_with_condition({"Student1":1, "Student2": 4}))
+
+# isinstance() works similar to type()
+# isinstance(value, int)
+# type(value) == int
---
+++ random/general.py
@@ -0,0 +1,1 @@
+# Python return None if return statement is not given in a function
